435cd439e6e215a1f17520029ef9b925ea279240
Mihai Raducanu <raducanu@gmail.com>
2019-08-01 23:36:11 +0300

    init

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Main = () => <div>Hello Main</div>
+
+export default Main
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const C = () => <div>Hello</div>
+
+ReactDOM.render(<C />, document.getElementById('index'))
